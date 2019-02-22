6e1eb7988c1effb98c8471d8b443292f193fe518
Michael Krohn <michaelkrohn@zipcoders-MacBook-Pro-96.local>
2019-02-22 09:23:19 -0500

    PET PET PET

--- src/test/java/io/zipcoder/ApplicationTest.java
+++ src/test/java/io/zipcoder/ApplicationTest.java
@@ -1,7 +1,32 @@
 package io.zipcoder;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ApplicationTest {
 
+@Test
 
+    public void howManyPetsTest(){
+    //Given
+    Application application = new Application();
+
+    List<Pet> expectedPetList = new ArrayList();
+    expectedPetList.add(new Dog("Bobby"));
+    expectedPetList.add(new Cat("Jawn"));
+    expectedPetList.add(new Dinosaur("Pat"));
+
+
+    //When
+
+    List<Pet> actualPetsList = application.getPeList();
+
+    //Then
+    Assert.assertTrue(expectedPetList.equals(actualPetsList));
+
+}
 }
